f58bcbd98721f04373b7a82382ed0ba6dee194a3
贾丽鹏 <1094425279@qq.com>
2026-01-22 17:15:44 +0800

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Alert, Button } from "react-native";
 
 export default function App() {
+  const onPress = () => {
+    Alert.alert("just do it!");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Nike</Text>
+      <Text style={styles.bigBlue}>just do it!</Text>
+      <Image
+        source={{
+          uri: "https://p3-flow-imagex-sign.byteimg.com/tos-cn-i-a9rns2rl98/rc_gen_image/0cd64693fa2a41fcb37c8e585e5880a9.jpeg~tplv-a9rns2rl98-downsize_watermark_1_5_b.png?lk3s=8e244e95&rcl=2026012114132556BE5D62C9FDD2386F80&rrcfp=ddbb2dc7&x-expires=2084336021&x-signature=nyjvad452njdw1hkKV9e7rvrArM%3D",
+        }}
+        style={{ width: 400, height: 400 }}
+      />
+      <Button onPress={onPress} title="Press Me" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  bigBlue: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 30,
   },
 });
---
+++ app/index.jsx
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View, Image, Alert } from "react-native";
+import { Link } from "expo-router";
+
+import Logo from "../assets/img/logo_light.png";
+
+const Home = () => {
+  const handlePress = () => {
+    Alert.alert("mfk!");
+  };
+  return (
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      <Text style={styles.title}>The Number 1</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+      {/* <View style={styles.card}>
+        <Text onPress={handlePress}>Hello, this is a card</Text>
+      </View>*/}
+      <Link href="/about">About Page</Link>
+      <Link href="/contact">Contact Page</Link>
+    </View>
+  );
+};
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#e0dfe8",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    marginVertical: 20,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0,0,0,0.1)",
+  },
+});
